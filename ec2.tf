e706569dfce2f0d4a8783d283d781ff75a534a7d
lintocjose22 <lintocjose22@gmail.com>
2023-05-08 12:49:53 -0400

    teraformtest

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -7,5 +7,5 @@ resource "aws_instance" "web" {
   }
 }
 output "instance_ip_addr" {
-  value = aws_instance.server.private_ip
+  value = aws_instance.frontend.public_ip
 }
