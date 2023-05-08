348cdb0691d56309d51fbf7b0d09734251f0a34e
lintocjose22 <lintocjose22@gmail.com>
2023-05-08 13:15:49 -0400

    teraformtest

--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,6 @@ resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
-  tags = {
-    Name = "linto"
-  }
 }
 output "instance_ip_addr" {
   value = aws_instance.frontend.public_ip
