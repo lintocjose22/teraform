3d3988192c9c71763b1a88de2f0b3f94dca56241
lintocjose22 <lintocjose22@gmail.com>
2023-05-08 13:27:50 -0400

    teraformtest

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,22 @@ resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
+  tags = {
+    Name = "frontend"
+  }
+}
+output "instance_ip_addr" {
+  value = aws_instance.frontend.public_ip
+}
+
+
+resource "aws_instance" "cart" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
 }
 output "instance_ip_addr" {
   value = aws_instance.frontend.public_ip
